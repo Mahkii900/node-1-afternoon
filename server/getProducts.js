f0631e34db98695b8ce045fe8f748bc5266c0667
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-12 15:50:41 -0600

    final commit

--- server/getProducts.js
+++ server/getProducts.js
@@ -2,6 +2,12 @@
 const products = require('../products.json')
 
 //handler here
-const getProducts = (req, res) => {res.status(200).send(products)}
+const getProducts = (req, res) => {
+    if (req.query.price) {
+        const prices = products.filter(item => item.price >= parseInt(req.query.price))
+        return res.status(200).send(prices)
+    } else {
+        res.status(200).send(products)}
+    }
 
 module.exports = getProducts
